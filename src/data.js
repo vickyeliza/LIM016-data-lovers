c909baab4bdc19daf3a916525eaf0cc1d2936b73
unknown <velizabeth.castilla@gmail.com>
2021-11-22 21:48:10 -0500

    enlace de data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,12 @@
-// estas funciones son de ejemplo
+import data from "./data/ghibli/ghibli.js";
 
-export const example = () => {
-  return 'example';
+//permite traer la funcion para poder ordenar toda la data
+
+
+export const sortfunction = (sortItemsValue,data) => {
+  switch ("sortItemsValue") {
+    case "score":
+  };
 };
 
 export const anotherExample = () => {
